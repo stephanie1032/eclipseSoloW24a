fa52080e708f6507ad02900ffc8ce30f0220adb3
stephanie1032 <snworgu@emich.edu>
2025-07-10 12:47:49 -0400

    tmp: prepare for 4.8 screenshot

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Step 4.8 screenshot test
 		System.out.println("This program rocks!");
 
 	}
